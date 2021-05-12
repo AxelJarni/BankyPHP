7b8c84c615433fcb7bba6ae47598b4591ccfe42d
Axel Jarnigon <axel.jarnigon@gmail.com>
2021-05-12 15:13:39 +0200

    calling accounts from php file

---
+++ comp/footer.php
@@ -0,0 +1,5 @@
+<footer class="jumbotron jumbotron-fluid bg-secondary text-white py-4 fixed-bottom">
+    <div class="container col-12">
+      <p class="text-end col-12">Une application développée par Axel JARNIGON pour Bankyy©</p>
+    </div>
+  </footer>
